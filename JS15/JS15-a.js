1e63f9139056ec8df7b5163a9e738590b8bdbe05
LeaLezcano <leolezcano@hotmail.com>
2024-02-15 12:54:56 +0100

    JS15

--- JS15/JS15-a.js
+++ JS15/JS15-a.js
@@ -1,10 +1,8 @@
 function calcular() {
     var bebida = document.getElementsByTagName("LI");
     console.log(bebida);
-    for (i=0; bebida.length; i++) {
-        if (bebida[i]===bebidaelegida){
+    if (bebida[i]==bebidaelegida){
             alert("Tu elección es: " +bebida[i].innerHTML);
-         }
     }
 }
 var bebidaelegida = prompt("elija un numero");
